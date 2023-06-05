051a299854e63d1563c4269c5387d92b8bac42df
leeHua1010 <im.leeHua@outlook.com>
2023-06-05 17:34:55 +0800

    chore: setup workflow

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import baostock as bs
 import stock_x
+import os
 
+index_codes = ['H30094','931468','931056']
 
 bs.login()
 
-stock_x.download_stocks_yield_data("H30094")
+for index_code in index_codes: 
+  if not os.path.exists(f'./roe_data/{index_code}.csv'):
+    stock_x.download_stocks_roe_data(index_code)
+
+  stock_x.download_stocks_yield_data(index_code)
 
 bs.logout()
